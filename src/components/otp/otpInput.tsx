cea54d38956b643500deaf08989bea51da6728e2
Vamozzz <sandeepmishra@vaamoz.com>
2024-03-20 19:36:04 +0530

    added otp page and implemented logic

---
+++ src/components/otp/otpInput.tsx
@@ -0,0 +1,80 @@
+import React, {
+  useRef,
+  useState,
+  ChangeEvent,
+  KeyboardEvent,
+  useEffect,
+} from "react";
+
+const OtpInput: React.FC<{ length?: number; onOtpSubmit?: () => void }> = ({
+  length = 4,
+  onOtpSubmit = () => {},
+}) => {
+  const [otp, setOtp] = useState<string[]>(new Array(length).fill(""));
+  const inputRef = useRef<HTMLInputElement[]>([]);
+
+  useEffect(() => {
+    if (inputRef.current[0]) {
+      inputRef.current[0].focus();
+    }
+  }, []);
+
+  const handleChange = (index: number, e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    // if (isNaN(parseInt(value))) return;
+    const newOtp = [...otp];
+    newOtp[index] = value.substring(value.length - 1);
+    setOtp(newOtp);
+
+    if (value && index < length - 1 && inputRef.current[index + 1]) {
+      inputRef.current[index + 1].focus();
+    }
+    // const combinedOtp = newOtp.join("").length === length;
+  };
+  // const validation =(value)=>{
+  //   const regex=
+  // if(vlaue)
+  // }
+
+  const handleKeyDown = (index: number, e: KeyboardEvent<HTMLInputElement>) => {
+    if (
+      e.key === "Backspace" &&
+      index > 0 &&
+      !otp[index] &&
+      inputRef.current[index - 1]
+    ) {
+      console.log("backspace is clicked");
+
+      e.preventDefault();
+      const newOtp = [...otp];
+      // Clear the current input and move focus to the previous input
+      newOtp[index] = "";
+      setOtp(newOtp);
+      if (index > 0) {
+        inputRef.current[index - 1]?.focus();
+      }
+    }
+  };
+
+  return (
+    <div>
+      {otp.map((value, index) => {
+        return (
+          <input
+            ref={(input) => {
+              inputRef.current[index] = input as HTMLInputElement;
+            }}
+            key={index}
+            type="number"
+            value={value}
+            onChange={(e) => handleChange(index, e)}
+            onKeyDown={(e) => handleKeyDown(index, e)}
+            className="bg-white w-[40px] h-[40px] m-2 rounded-lg text-center"
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default OtpInput;
--- src/components/popup/otpPopup.tsx
+++ src/components/popup/otpPopup.tsx
@@ -3,11 +3,9 @@ import "./popup.css";
 import "../../index.css";
 import CustomInput from "../customInput/customInput";
 import OTPInput from "react-otp-input";
+import OtpInput from "../otp/otpInput";
 
 const OtpPopup = () => {
-  const [otp, setOtp] = useState("");
-  const num = "8169606880";
-
   return (
     <div className="w-[90%] lg:w-auto m-auto p-4  bg-redColor-redPrime lg:p-10 rounded-2xl text-center centre text-poppins">
       <div className="flex flex-col items-center gap-3 ">
@@ -18,39 +16,8 @@ const OtpPopup = () => {
           We have sent you a 4 digit code. Please enter here to Verify your
           Number.
         </p>
-        <CustomInput
-          disabled={true}
-          htmlFor="first_name"
-          placeholder="Enter First Name"
-          value={`+91 ${num}`}
-        />
       </div>
-      <div className="flex gap-2 px-20">
-        <CustomInput
-          maxLength={1}
-          htmlFor="first_name"
-          placeholder="Enter First Name"
-          value={"1"}
-        />
-        <CustomInput
-          maxLength={1}
-          htmlFor="first_name"
-          placeholder="Enter First Name"
-          value={"1"}
-        />
-        <CustomInput
-          maxLength={1}
-          htmlFor="first_name"
-          placeholder="Enter First Name"
-          value={"1"}
-        />
-        <CustomInput
-          maxLength={1}
-          htmlFor="first_name"
-          placeholder="Enter First Name"
-          value={"1"}
-        />
-      </div>
+      <OtpInput />
       <div>
         <p>Didn’t Receive Code? Get a New one</p>
       </div>
